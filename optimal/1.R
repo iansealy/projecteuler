37502a3511a1b5df9666992a7c9987a9ac54e6bc
Ian Sealy <github@iansealy.com>
2015-01-12 13:17:26 +0000

    R optimal solution to problem 1.

---
+++ optimal/1.R
@@ -0,0 +1,11 @@
+# Multiples of 3 and 5
+
+# Constants
+MAX_NUM = 1000
+
+sum_sequence <- function(step) {
+    last_int <- (MAX_NUM - 1) %/% step
+    return(step * last_int * ( last_int + 1) / 2)
+}
+
+cat(sum_sequence(3) + sum_sequence(5) - sum_sequence(15), fill=TRUE)
