b8093db6c6f457d2ab655067cf4fda9c2e45d9a8
Ian Sealy <github@iansealy.com>
2015-02-20 08:18:24 +0000

    R solution to problem 10.

---
+++ 10.R
@@ -0,0 +1,47 @@
+# Summation of primes
+
+Args  <- commandArgs()
+limit <- ifelse(is.na(Args[6]), 2000000, as.numeric(Args[6]))
+
+is_prime <- function(num) {
+    if ( num == 1 ) { # 1 isn't prime
+        return(FALSE)
+    }
+    if ( num < 4 ) { # 2 and 3 are prime
+        return(TRUE)
+    }
+    if ( num %% 2 == 0 ) { # Odd numbers aren't prime
+        return(FALSE)
+    }
+    if ( num < 9 ) { # 5 and 7 are prime
+        return(TRUE)
+    }
+    if ( num %% 3 == 0 ) { # Numbers divisible by 3 aren't prime
+        return(FALSE)
+    }
+
+    num_sqrt <- as.integer(sqrt(num))
+    factor <- 5
+    while ( factor <= num_sqrt ) {
+        if ( num %% factor == 0 ) { # Primes greater than three are 6k-1
+            return(FALSE)
+        }
+        if ( num %% (factor + 2) == 0 ) { # Or 6k+1
+            return(FALSE)
+        }
+        factor <- factor + 6
+    }
+    return(TRUE)
+}
+
+candidate <- 3
+sum <- 2 # 2 is a prime
+
+while ( candidate < limit ) {
+    if ( is_prime(candidate) ) {
+        sum <- sum + candidate
+    }
+    candidate <- candidate + 2
+}
+
+cat(sum, fill=TRUE)
