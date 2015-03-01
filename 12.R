05210b406ea9a413156ee5ba0fbf0ecf3c789b08
Ian Sealy <github@iansealy.com>
2015-03-01 12:12:58 +0000

    R solution to problem 12.

---
+++ 12.R
@@ -0,0 +1,28 @@
+# Highly divisible triangular number
+
+Args     <- commandArgs()
+divisors <- ifelse(is.na(Args[6]), 500, as.numeric(Args[6]))
+
+get_factors <- function(number) {
+    factors <- c(1, number)
+
+    for (i in seq.int(2, as.integer(sqrt(number)))) {
+        if ( number %% i == 0 ) {
+            factors <- c(factors, i, number %/% i)
+        }
+    }
+
+    return(factors)
+}
+
+ordinal <- 1
+triangle_number <- 1
+num_factors <- 1
+
+while ( num_factors <= divisors ) {
+    ordinal <- ordinal + 1
+    triangle_number <- triangle_number + ordinal
+    num_factors <- length(get_factors(triangle_number))
+}
+
+cat(triangle_number, fill=TRUE)
