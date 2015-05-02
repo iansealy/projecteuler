3f4d6e634fa3b57b01633469ea1a73ffbb721f65
Ian Sealy <github@iansealy.com>
2015-05-02 20:45:20 +0100

    Python solution to problem 31.

---
+++ 31.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+"""This script solves the Project Euler problem "Coin sums". The problem is:
+How many different ways can 200p be made using any number of coins?
+"""
+
+from functools import lru_cache
+
+
+def main():
+    """Coin sums"""
+
+    # Constants
+    COINS = (200, 100, 50, 20, 10, 5, 2, 1)
+
+    print(change(200, COINS))
+
+
+@lru_cache()
+def change(money, coins):
+    """Recursively calculate number of ways to make change"""
+
+    if money < 0:
+        return 0
+    if money == 0:
+        return 1
+
+    count = 0
+    while(coins):
+        count += change(money - coins[0], coins)
+        coins = coins[1:]
+
+    return count
+
+if __name__ == '__main__':
+    main()
