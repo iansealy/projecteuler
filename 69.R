8baa0579119aed5db841fe497bc3b6e1fcd16465
Ian Sealy <github@iansealy.com>
2015-09-09 09:31:13 +0100

    R solution to problem 69.

---
+++ 69.R
@@ -0,0 +1,50 @@
+# Totient maximum
+
+Args  <- commandArgs()
+limit <- ifelse(is.na(Args[6]), 1000000, as.numeric(Args[6]))
+
+is_prime <- function(num) {
+    if ( num == 1 ) { # 1 isn't prime
+        return(FALSE)
+    }
+    if ( num < 4 ) { # 2 and 3 are prime
+        return(TRUE)
+    }
+    if ( num %% 2 == 0 ) { # Odd numbers aren't prime
+        return(FALSE)
+    }
+    if ( num < 9 ) { # 5 and 7 are prime
+        return(TRUE)
+    }
+    if ( num %% 3 == 0 ) { # Numbers divisible by 3 aren't prime
+        return(FALSE)
+    }
+
+    num_sqrt <- as.integer(sqrt(num))
+    factor <- 5
+    while ( factor <= num_sqrt ) {
+        if ( num %% factor == 0 ) { # Primes greater than three are 6k-1
+            return(FALSE)
+        }
+        if ( num %% (factor + 2) == 0 ) { # Or 6k+1
+            return(FALSE)
+        }
+        factor <- factor + 6
+    }
+    return(TRUE)
+}
+
+n <- 1
+prime <- 1
+while ( TRUE ) {
+    prime <- prime + 1
+    if ( !is_prime(prime) ) {
+        next
+    }
+    if ( n * prime > limit ) {
+        break
+    }
+    n <- n * prime
+}
+
+cat(n, fill=TRUE)
