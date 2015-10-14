10a5c9504eb11542900635512d445c9a7412ab52
Ian Sealy <github@iansealy.com>
2015-10-14 18:09:12 +0100

    Python solution to problem 78.

---
+++ 78.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+"""This script solves the Project Euler problem "Coin partitions". The problem
+is: Find the least value of n for which p(n) is divisible by one million.
+"""
+
+from __future__ import division
+import argparse
+from functools import lru_cache
+
+
+def main(args):
+    """Coin partitions"""
+
+    n = 1
+    while partitions(n):
+        n += 1
+
+    print(n)
+
+
+@lru_cache(maxsize=None)
+def partitions(n):
+
+    if n == 0:
+        return(1)
+
+    parts = 0
+    k = 1
+    while True:
+        pent = pentagonal(k)
+        if n - pent < 0:
+            break
+        parts += int(pow(-1, k - 1)) * partitions(n - pent)
+        parts = parts % args.divisor
+        if k > 0:
+            k = -k
+        else:
+            k = -k + 1
+
+    return(parts)
+
+
+@lru_cache(maxsize=None)
+def pentagonal(n):
+    return((3 * n * n - n) // 2)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Coin partitions')
+    parser.add_argument(
+        'divisor', metavar='DIVISOR', type=int, default=1000000, nargs='?',
+        help='The target divisor')
+    args = parser.parse_args()
+
+    main(args)
