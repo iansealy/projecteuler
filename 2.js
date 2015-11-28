fdc4b357ad1b387edebdd147290badbfad52e0bf
Ian Sealy <git@iansealy.com>
2015-11-28 20:30:43 +0000

    Reformat.

--- 2.js
+++ 2.js
@@ -8,9 +8,9 @@ var fib1 = 1;
 var fib2 = 1;
 var sum_even = 0;
 while (fib2 < MAX_FIB) {
-    var tmp = fib2
-    fib2 = fib1 + fib2
-    fib1 = tmp
+    var tmp = fib2;
+    fib2 = fib1 + fib2;
+    fib1 = tmp;
     if (fib1 % 2 == 0) {
         sum_even += fib1;
     }
