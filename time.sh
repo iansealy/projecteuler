a879beaf2ce09cdb7323c3ce1fec4e8e99a7d208
Ian Sealy <git@iansealy.com>
2015-11-28 20:33:34 +0000

    Script to time all solutions.

---
+++ time.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+function timing {
+    for file in `ls optimal/*.$1 *.$1 | sort -V`; do
+        echo $file | sed -e 's/\..*//'
+        (time -p $2 $file) 2>&1 | grep -vE '^(user|sys)' | sed -e 's/^real //'
+    done | paste - - - > time-$1.txt
+}
+
+timing pl perl
+timing py python
+timing R  Rscript
+timing js node
