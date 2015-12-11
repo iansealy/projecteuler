a13517c2eae4002cb2a2fb3dbaa553bb0261533f
Ian Sealy <git@iansealy.com>
2015-12-11 11:06:58 +0000

    JavaScript solution to problem 15.

---
+++ 15.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+// Lattice paths
+
+var program = require('commander');
+
+program
+    .version('0.1.0')
+    .description('Lattice paths')
+    .option('-g, --grid_size <int>',
+        'The size of the grid', Number, 20)
+    .parse(process.argv);
+
+var size = program.grid_size;
+console.log(Math.round(factorial(size + size) / Math.pow(factorial(size), 2)));
+
+function factorial(number) {
+    var factorial = 1;
+
+    while (number > 1) {
+        factorial *= number;
+        number--;
+    }
+
+    return factorial;
+}
