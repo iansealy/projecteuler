d137abd310c0acbda4e8e19dc4a5881b2d3af3ab
Ian Sealy <git@iansealy.com>
2015-12-21 00:02:27 +0000

    JavaScript solution to problem 22.

---
+++ 22.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+// Names scores
+
+var NAMES_URL = 'https://projecteuler.net/project/resources/p022_names.txt';
+
+var request = require('request');
+request(NAMES_URL, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        score_names(body.replace(/"/g, "").split(","));
+    }
+});
+
+function score_names(names) {
+    names.sort();
+
+    var total = 0;
+
+    for (var i = 0; i < names.length; i++) {
+        var value = 0;
+        for (var j = 0; j < names[i].length; j++) {
+            value += names[i].charCodeAt(j) - 64;
+        }
+        total += (i + 1) * value;
+    }
+
+    console.log(total);
+}
