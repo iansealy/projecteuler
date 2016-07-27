2ba01e98a7265b395c06649c0aecee1c1758368a
Ian Sealy <git@iansealy.com>
2016-07-27 09:00:30 +0100

    Python solution to problem 114.

---
+++ 114.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+"""This script solves the Project Euler problem "Counting block combinations
+I". The problem is: How many ways can a row measuring fifty units in length be
+filled?
+"""
+
+import argparse
+try:
+    from functools import lru_cache
+except ImportError:
+    from functools32 import lru_cache
+
+
+def main(args):
+    """Counting block combinations I"""
+
+    # Constants
+    global MIN_BLOCK
+    MIN_BLOCK = 3
+
+    print(ways(args.units))
+
+
+@lru_cache()
+def ways(total_length):
+    count = 1
+
+    if total_length < MIN_BLOCK:
+        return count
+
+    for start in range(total_length - MIN_BLOCK + 1):
+        for block_length in range(MIN_BLOCK, total_length - start + 1):
+            count += ways(total_length - start - block_length - 1)
+
+    return count
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Counting block combinations I')
+    parser.add_argument(
+        'units', metavar='UNITS', type=int, default=50, nargs='?',
+        help='Maximum value of n as an exponent of 10')
+    args = parser.parse_args()
+
+    main(args)
