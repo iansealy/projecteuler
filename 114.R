397fda5221002c7b5c3b67ea939ab9bcfde73f7d
Ian Sealy <git@iansealy.com>
2016-07-28 10:11:48 +0100

    R solution to problem 114.

---
+++ 114.R
@@ -0,0 +1,29 @@
+# Counting block combinations I
+
+library(memoise)
+
+#options(scipen=100)
+
+Args  <- commandArgs()
+units <- ifelse(is.na(Args[6]), 50, as.numeric(Args[6]))
+
+# Constants
+MIN_BLOCK <- 3
+
+ways <- memoise(function(total_length) {
+    count <- 1
+
+    if ( total_length < MIN_BLOCK ) {
+        return(count)
+    }
+
+    for ( start in seq.int(0, total_length - MIN_BLOCK) ) {
+        for ( block_length in seq.int(MIN_BLOCK, total_length - start) ) {
+            count <- count + ways(total_length - start - block_length - 1)
+        }
+    }
+
+    return(count)
+})
+
+cat(ways(units), fill=TRUE)
