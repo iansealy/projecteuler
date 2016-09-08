8453db7c06f986d575f49e2b524a09f79a58f88f
Ian Sealy <git@iansealy.com>
2016-09-08 10:04:11 +0100

    R solution to problem 123.

---
+++ 123.R
@@ -0,0 +1,48 @@
+# Prime square remainders
+
+Args  <- commandArgs()
+limit <- ifelse(is.na(Args[6]), 1e10, as.numeric(Args[6]))
+
+get_primes_up_to <- function(limit) {
+    sieve_bound <- (limit - 1) %/% 2 # Last index of sieve
+    sieve <- rep(FALSE, sieve_bound)
+    cross_limit <- (floor(sqrt(limit)) - 1) %/% 2
+
+    i <- 1
+    while ( i <= cross_limit ) {
+        if (!sieve[i]) {
+            # 2 * $i + 1 is prime, so mark multiples
+            j <- 2 * i * (i + 1)
+            while ( j <= sieve_bound ) {
+                sieve[j] <- TRUE
+                j <- j + 2 * i + 1
+            }
+        }
+        i <- i + 1
+    }
+
+    primes <- rep(0, sieve_bound)
+    for ( i in seq(1, sieve_bound) ) {
+        if ( !sieve[i] ) {
+            primes[i] <- 2 * i + 1
+        }
+    }
+
+    return(c(2, primes[primes > 0]))
+}
+
+primes <- get_primes_up_to(sqrt(limit) * 10)
+
+n <- 0
+for ( prime in primes ) {
+    n <- n + 1
+    if ( n %% 2 == 0 ) {
+        next
+    }
+    r <- prime * n * 2
+    if ( r > limit ) {
+        break
+    }
+}
+
+cat(n, fill=TRUE)
